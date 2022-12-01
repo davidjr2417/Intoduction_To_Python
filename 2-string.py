e083ae8a85036f7d374c5918c09993f837bb884e
davidjr2417 <david@hiddengeniusproject.org>
2022-11-30 18:26:49 -0800

    first commit

---
+++ 2-string.py
@@ -0,0 +1,3 @@
+#print() - Print Function 
+#String Conacatenation - Combine 2 or more strings together with (+) sign
+print("Hello David" + " " +"Malone" )
---
+++ 3-input.py
@@ -0,0 +1,4 @@
+#print() - Function Prints Out Information To Console
+#String Conacatenation - Combine 2 or more strings together with (+) sign
+#input() - Function Get information from User
+print("Hello " + input("What Is Your Name?"))
